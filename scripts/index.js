9919f81df83ba71bd3511729805277eeac5b5598
kadamyash <yash11398@gmail.com>
2021-10-04 15:36:39 +0530

    'header'

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,28 @@
 if(localStorage.getItem('theme')!==null){
     const theme = localStorage.getItem('theme');
     document.getElementById('main').className = theme;
+    if(theme==='dark'){
+        document.getElementById('headerbar').setAttribute.content = '#353535';
+    }
+    else if(theme==='dark'){
+        document.getElementById('headerbar').setAttribute.content = '#f0efeb';
+    }
 }
 else{
     document.getElementById('main').className = 'light';
+    document.getElementById('headerbar').setAttribute.content = '#f0efeb';
 }
 
 function switchTheme(){
     const element = document.getElementById('main');
     if(element.className==='light'){
         element.className='dark';
+        document.getElementById('headerbar').setAttribute.content = '#353535';
         localStorage.setItem('theme', 'dark');
     }
     else{
         element.className='light';
+        document.getElementById('headerbar').setAttribute.content = '#f0efeb';
         localStorage.setItem('theme', 'light');
     }
 }
